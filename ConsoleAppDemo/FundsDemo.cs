ba0c6d001ad61ecc96eff27813b3731307b11461
gStoyan <grancharovstoyan@gmail.com>
2022-09-08 10:48:48 +0300

    Initial

---
+++ ConsoleAppDemo/FundsDemo.cs
@@ -0,0 +1,76 @@
+﻿namespace ConsoleAppDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class FundsDemo
+    {
+        public static void Start()
+        {
+            Console.WindowHeight = 50;
+            Console.BufferHeight = 50;
+            Console.WindowWidth = 160;
+            Console.BufferWidth = 160;
+            Console.Clear();
+
+            var operations = new List<string>() { "deposit", "checkinvome", "save" };
+            int pageSize = 4;
+            int pointer = 1;
+
+            while (true)
+            {
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.Clear();
+                Console.WriteLine("Choose your operation");
+
+                int current = 1;
+                foreach (var operation in operations)
+                {
+                    if (current == pointer)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    Console.WriteLine(operation);
+                    current++;
+                }
+                var key = Console.ReadKey();
+                switch (key.Key.ToString())
+                {
+                    case "Enter":
+
+                        return;
+
+                    case "UpArrow":
+                        if (pointer > 1)
+                        {
+                            pointer--;
+                        }
+
+                        break;
+                    case "DownArrow":
+                        if (pointer < pageSize)
+                        {
+                            pointer++;
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleAppDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Clear the screen, then save the top and left coordinates.
+
+
+using ConsoleAppDemo;
+
+
+FundsDemo.Start();
